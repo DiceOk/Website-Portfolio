38ce7ce8476ca59aee9b8e6cd2534b47755bf3e2
DiceOk <daisukehamo@gmail.com>
2025-11-03 18:55:12 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import HireMe from "./pages/HireMe";
-import Layout from "./Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="hireme" element={<HireMe />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
